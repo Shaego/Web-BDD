60194c1444f53191e8179e50b5dce3b43623ae12
shaego <babsbouilletgmail.com>
2019-04-04 10:45:24 -0400

    BdAeroport + BdEdition 90% done

---
+++ LabAeroport/statistiques.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Buzz Lightyear Airways</title>
+    <link rel="stylesheet"  href="style.css"/>
+</head>
+<header id="header">
+    <?php include 'header.php'; ?>
+</header>
+<body>
+<?php include 'menu.php'; ?>
+</body>
+<footer>
+    <?php include 'footer.php'; ?>
+</footer>
+</html>
